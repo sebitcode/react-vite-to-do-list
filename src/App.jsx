a7f0897923ae26b9007d69fe04aadead4aa6629d
sebitcode <sebitcode@gmail.com>
2025-02-13 15:08:13 -0500

    to do list with react and vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ToDoList from './Components/ToDoList'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <ToDoList />
+    </>
+  )
+}
+
+export default App
